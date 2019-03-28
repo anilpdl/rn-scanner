ada085b43678e236981c976afa9bf3e2f20c0c74
pdlanl <darkz.anil@gmail.com>
2019-03-28 13:00:38 +0545

    file fix

--- android/src/main/java/com/reactlibrary/RNReactNativeAndroidScannerModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeAndroidScannerModule.java
@@ -8,7 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -72,7 +72,7 @@ public class RNReactNativeAndroidScannerModule extends ReactContextBaseJavaModul
     Intent intent = new Intent(getReactApplicationContext(), ScanActivity.class);
     intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, preference);
       currentActivity.startActivityForResult(intent, REQUEST_CODE);
-  } 
+  }
 
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
 
